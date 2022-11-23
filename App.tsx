e534834d92e105ef7285d0b2847018341941e8a7
jrmart12 <rodolfojose@live.com>
2022-11-23 12:12:52 -0600

    redux

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Banner} from 'LinkerStudio/core/components/atoms';
+import {Provider} from 'react-redux';
 export const App: React.FC = () => (
   <div>
     <h1>React Typescript webpack babel prettier eslint css sass</h1>
